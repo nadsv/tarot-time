f74428175628807c593acf03956555e010bf4955
Nadezhda Sve <nadsvezh@yandex.ru>
2023-08-22 12:49:03 +0300

    decorative panel created

---
+++ src/components/readings/DecorativePanel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="container text-dark"
+    :style="{
+      'background-color': panelColor,
+    }"
+  >
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  panelColor?: string;
+}
+withDefaults(defineProps<Props>(), {
+  panelColor: '#7f3aad"',
+});
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-basis: 599px;
+  height: calc(100vh - 130px);
+  min-height: 500px;
+  opacity: 0.8;
+  border-image-slice: 27 27 27 27;
+  border-image-width: 15px 15px 15px 15px;
+  border-image-outset: 8px 8px 8px 8px;
+  border-image-repeat: stretch stretch;
+  border-image-source: url(./src/assets/bi-panel-2.png);
+  border-style: solid;
+  margin: 15px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,40 +1,27 @@
 <template>
   <q-page class="row items-strech justify-evenly bckgrnd">
-    <div class="panel" style="background-color: bisque">Hello!</div>
-    <div class="panel">
-      <tarot-deck active :todos="todos" :meta="meta"></tarot-deck>
-    </div>
-    <div class="panel" style="background-color: rgb(182, 119, 41)">Buy!</div>
+    <decorative-panel panel-color="#7f3aad"
+      ><tarot-deck></tarot-deck
+    ></decorative-panel>
+    <decorative-panel panel-color="#ffe4c4">Hello!</decorative-panel>
+    <decorative-panel panel-color="#f5e274">It is a rainbow!</decorative-panel>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { Todo, Meta } from 'components/models';
 import TarotDeck from 'src/components/TarotDeck.vue';
+import DecorativePanel from 'src/components/readings/DecorativePanel.vue';
 import { ref } from 'vue';
 import { useQuasar } from 'quasar';
 
 const $q = useQuasar();
 $q.dark.set(true);
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-]);
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
 </script>
 
 <style scoped>
 .bckgrnd {
   background-image: url(./src/assets/bg-main.jpg);
   background-size: cover;
-}
-
-.panel {
-  flex-basis: 599px;
 }
 </style>
