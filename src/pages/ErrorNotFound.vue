0c273ba513ec50f5ec0c37cb77b75293a70bd30e
Veranona <nadsvezh@yandex.ru>
2023-10-05 14:39:59 +0300

    ErrorNotFound changed

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center bg-primary"
+    class="fullscreen text-white text-center q-pa-md flex flex-center bckgrnd"
   >
     <div>
-      <div style="font-size: 30vh">404</div>
+      <div class="error">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Страница не найдена</div>
+      <div class="text-h2" >Вы заблудились в мире магии</div>
 
       <q-btn
         class="q-mt-xl"
-        color="white"
-        text-color="blue"
+        color="accent"
+        text-color="dark"
         unelevated
         to="/"
         label="На главную"
@@ -21,3 +21,16 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<style scope>
+  .bckgrnd {
+      background-image: url(/assets/bg-page.jpg);
+      background-repeat: repeat;
+    }
+  .error {
+      font-size: 30vh;
+      font-family: 'vinqueregular';
+      text-transform: lowercase;
+    }
+
+</style>
